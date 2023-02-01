a5fce9a0e50b899be189333cb48ec4558eccb329
Alexis Abda <alexis.abda@hotmail.com>
2023-02-01 16:03:56 +0100

    Add error case

--- includes/errors.h
+++ includes/errors.h
@@ -6,7 +6,7 @@
 /*   By: aabda <aabda@student.s19.be>               +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/30 22:58:19 by aabda             #+#    #+#             */
-/*   Updated: 2023/01/31 22:53:44 by aabda            ###   ########.fr       */
+/*   Updated: 2023/02/01 15:38:52 by aabda            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,12 +14,21 @@
 # define ERRORS_H
 
 # define ERR_MALLOC "Memory allocation failure !\n"
-# define ERR_NOT_ENOUGH_PHILO "You must have minimum one philosopher !\n"
 # define ERR_INVALID_ARGS "Invalid number of argument(s), \
 you must run the program as follows :\n\
 \t./philo number_of_philosophers time_to_die time_to_eat time_to_sleep \
 [number_of_times_each_philosopher_must_eat]\n"
+# define ERR_NOT_ENOUGH_PHILO "You must have minimum one philosopher \
+(argv[1]) !\n"
 # define ERR_NOT_ENOUGH_TIME_TO_DIE "The \"time to die\" (argv[2]) \
-must not be 0 or less than 0 !\n"
+must not be \033[0;31;1m0\033[0;0m or less than \033[0;31;1m0\033[0;0m !\n"
+# define ERR_NOT_ENOUGH_TIME_TO_EAT "The \"time to eat\" (argv[3]) \
+must not be \033[0;31;1m0\033[0;0m or less than \033[0;31;1m0\033[0;0m !\n"
+# define ERR_NOT_ENOUGH_TIME_TO_SLEEP "The \"time to sleep\" (argv[4]) \
+must not be \033[0;31;1m0\033[0;0m or less than \033[0;31;1m0\033[0;0m !\n"
+# define ERR_NOT_ENOUGH_MUST_EAT "The \"number of times each philosopher must \
+eat\" (argv[5]) \
+must not be \033[0;31;1m0\033[0;0m or less than \033[0;31;1m0\033[0;0m ! \
+(this arg is optionnal).\n"
 
 #endif
--- srcs/main.c
+++ srcs/main.c
@@ -6,7 +6,7 @@
 /*   By: aabda <aabda@student.s19.be>               +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/30 13:47:09 by aabda             #+#    #+#             */
-/*   Updated: 2023/01/31 22:50:31 by aabda            ###   ########.fr       */
+/*   Updated: 2023/02/01 15:28:52 by aabda            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,13 +16,19 @@ static int	ft_check_error(int res)
 {
 	if (res < 0)
 	{
-		ft_putstr_fd(2, "ERROR !\n");
+		ft_putstr_fd(2, "\033[0;31;1mERROR !\033[0m\n");
 		if (res == -1)
 			ft_putstr_fd(2, ERR_MALLOC);
 		if (res == -2)
 			ft_putstr_fd(2, ERR_NOT_ENOUGH_PHILO);
 		if (res == -3)
 			ft_putstr_fd(2, ERR_NOT_ENOUGH_TIME_TO_DIE);
+		if (res == -4)
+			ft_putstr_fd(2, ERR_NOT_ENOUGH_TIME_TO_EAT);
+		if (res == -5)
+			ft_putstr_fd(2, ERR_NOT_ENOUGH_TIME_TO_SLEEP);
+		if (res == -6)
+			ft_putstr_fd(2, ERR_NOT_ENOUGH_MUST_EAT);
 	}
 	return (res);
 }
