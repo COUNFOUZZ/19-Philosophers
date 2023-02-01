44afc180062d05ed6f624e1ff17c521e3052fcf0
Alexis Abda <alexis.abda@hotmail.com>
2023-02-01 15:04:25 +0100

    add function ft_check_args_error

--- srcs/utils.c
+++ srcs/utils.c
@@ -6,11 +6,40 @@
 /*   By: aabda <aabda@student.s19.be>               +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/30 20:05:24 by aabda             #+#    #+#             */
-/*   Updated: 2023/01/31 20:28:25 by aabda            ###   ########.fr       */
+/*   Updated: 2023/01/31 22:59:28 by aabda            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/philosophers.h"
+
+static void	ft_check_args_error(t_global *g)
+{
+	if (!g->params.nbr_philo || g->params.nbr_philo <= 0)
+	{
+		g->err_check = -2;
+		return ;
+	}
+	if (!g->params.time_to_die || g->params.time_to_die <= 0)
+	{
+		g->err_check = -3;
+		return ;
+	}
+	if (!g->params.time_to_eat || g->params.time_to_eat <= 0)
+	{
+		g->err_check = -4;
+		return ;
+	}
+	if (!g->params.time_to_sleep || g->params.time_to_sleep <= 0)
+	{
+		g->err_check = -5;
+		return ;
+	}
+	if (g->params.must_eat && g->params.must_eat <= 0)
+	{
+		g->err_check = -6;
+		return ;
+	}
+}
 
 static int	ft_parse_args(t_global *g, const char **argv)
 {
@@ -21,6 +50,7 @@ static int	ft_parse_args(t_global *g, const char **argv)
 	g->params.time_to_eat = ft_atoi(argv[3]);
 	g->params.time_to_sleep = ft_atoi(argv[4]);
 	g->params.must_eat = ft_atoi(argv[5]);
+	ft_check_args_error(g);
 	return (g->err_check);
 }
 
@@ -29,17 +59,9 @@ static void	ft_check_args(t_global *g)
 	int	i;
 
 	i = -1;
-	if (g->params.nbr_philo <= 0)
-	{
-		g->err_check = -2;
-		return ;
-	}
-	else
-	{
-		while (++i < g->params.nbr_philo)
-			ft_add_philo(g, i);
-		ft_add_prev_for_first_and_last(g);
-	}
+	while (++i < g->params.nbr_philo)
+		ft_add_philo(g, i);
+	ft_add_prev_for_first_and_last(g);
 }
 
 int	ft_init_struct(t_global *g, const char **argv)
@@ -48,5 +70,5 @@ int	ft_init_struct(t_global *g, const char **argv)
 		return (g->err_check);
 	ft_check_args(g);
 	ft_print_lst(g);
-	// printf("1 = %d\n2 = %d\n3 = %d\n4 = %d\n5 = %d\n", g->params.nbr_philo, g->params.time_to_die, g->params.time_to_eat, g->params.time_to_sleep, g->params.must_eat);
+	return (g->err_check);
 }
